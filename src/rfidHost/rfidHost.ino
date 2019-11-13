befea5230261a7ccb6abc79521fd888be4f31362
gabriel <gabriel.becker.59@gmail.com>
2019-11-13 00:30:03 -0300

    include esp firmware with mdns

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -2,9 +2,9 @@
 #include <SPI.h>
 #include <MFRC522.h>
  
-
 #define RST_PIN 5 
-    #define SS_PIN 53
+#define SS_PIN 53
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() 
